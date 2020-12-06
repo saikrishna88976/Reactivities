d255b7d9e1071d34ea8b97354d11475c740e6df3
Sai Krishna Bingi <saikrishna88976@gmail.com>
2020-12-06 20:58:24 +0530

    After Semnatic basic page

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
 class App extends Component {
   state = {
@@ -10,7 +10,6 @@ class App extends Component {
 
   componentDidMount() {
     Axios.get("http://localhost:5000/api/values").then((response) => {
-      console.log(response);
       this.setState({
         values: response.data,
       });
@@ -19,19 +18,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+        {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+        <ul>
+          
+        </ul>
       </div>
     );
   }
 }
 
 export default App;
-
